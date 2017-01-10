8ea5dcdf351c78761311b893deafb9cf03bf5e8a
u223699 <simon.erhardt@sbb.ch>
2017-01-10 10:47:53 +0100

    added check for atlassian tools

---
+++ check_atlassian.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ATLASSIAN_URL="$1"
+
+function responseTime {
+  curl -o /dev/null -s -L -w "%{time_total}" "$1"
+}
+
+# Check api status
+ATLASSIAN_STATE=$(curl -s -L "$1/status" 2>/dev/null)
+
+if [ "$ATLASSIAN_STATE" != '{"state":"RUNNING"}' ]; then
+  echo "Atlassian status endpoint has bad state."
+  exit 2
+fi
+
+# Check response time
+RESPONSE_TIME=$(responseTime "$ATLASSIAN_URL")
+if [ $(echo "$RESPONSE_TIME > 0" | bc) -eq 1 ]; then
+  echo "Atlassian OK|${RESPONSE_TIME}s"
+  if [ $(echo "$RESPONSE_TIME > 1" | bc) -eq 1 ]; then
+    exit 1
+  else
+    exit 0
+  fi
+else
+  echo "Atlassian NOK"
+  exit 2
+fi
